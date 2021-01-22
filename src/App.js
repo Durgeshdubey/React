32a23d0a8ba03f5d9f7fd0d06ab2966b861d3225
durgesh <durgesh.d@transfinite.one>
2021-01-22 23:33:12 +0530

    React Start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import Tweet from './Tweet';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+
+//  const [isRed, setRed] =  useState(false);
+//  const [count, setCount] = useState(0);
+
+  
+//  const Increment = () => {
+//    setCount(count+1);
+//    setRed(!isRed)
+  const [users, setUser] = useState([
+    {name:"GOku", message:"hello this is goku", likes:45},
+    {name:"Gohan", message: "piccolo is my dad", likes:45},
+    {name:"Vegeta", message:"Clownnnnnnn", likes:45}
+  ])
+
+
+
+ return (
+    <div className="app" >
+      {/* <h1 className={isRed ? 'red' : ''}>change my color</h1> */}
+     {/* <Tweet name="Goku"   message="This is a JOke" likes="200" />
+     <Tweet name="Vegeta" message="This is a Clown" likes="101" />
+     <Tweet name="Gohan"  message="This is a 12 supersayian" likes="110" />
+     <Tweet name="Trunks" message="Slash fieiza from middle" likes="210" /> */}
+{/* 
+     <button onClick={Increment}>Increment</button>
+     <h1>{count}</h1> */}
+     {users.map(user => (
+       <Tweet name={user.name} message={user.message} likes={user.likes}/>
+     ))}
     </div>
   );
 }
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import App from "./App.js";
+import Login from "./login.js";
+
+const Routes = [
+{
+    path:"/App", 
+    name:"App"
+},
+{
+    path:"/login",
+    name:"Login"
+}   
+
+]
+export default Routes
